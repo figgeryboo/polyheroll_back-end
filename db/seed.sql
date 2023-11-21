136cccf89c0ff1873e542a5e86264e835a39d726
Fig <bethanytrr@gmail.com>
2023-11-21 10:43:48 -0500

    changing charset

--- db/seed.sql
+++ db/seed.sql
@@ -2,9 +2,9 @@
 
 INSERT INTO dice_set (character_name, campaign, complete_set, is_in_dice_jail, character_level, d20, dice_color_theme)
 VALUES 
-  ('Citalá Hesperus', 'Wilds of Esola', true, false, 13, 6, 'purple'),
+  ('Citala Hesperus', 'Wilds of Esola', true, false, 13, 6, 'purple'),
   ('Faelyn Iltesto', 'Elysian Nights', true, false, 7, 3, 'pink'),
   ('Sahir Chaudhari', 'Sky Spires', false, true, 10, 1, 'bronze'),
   ('Tilly Pennybrook', 'Savvy', false, false, 1, 1, 'peach'),
   ('Gwynet Macdhu', 'Curse of Strahd', true, false, 4, 2, 'orange'),
-  ('Zhōu Shé Jiû', 'Sky Spires', true, true, 9, 1, 'green');
+  ('Zhou She Jiu', 'Sky Spires', true, true, 9, 1, 'green');
